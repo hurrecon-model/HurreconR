317d804642be8b67aebea3bf29bbb8218e980338
Boose <boose@fas.harvard.edu>
2025-11-22 10:01:18 -0500

    Update unit tests 6

--- tests/testthat/test_HurreconR.R
+++ tests/testthat/test_HurreconR.R
@@ -10,8 +10,6 @@ hur_path <- system.file("", package="HurreconR", mustWork=TRUE)
 tdir <- tempdir()
 
 dir.create(paste0(tdir, '/input'))
-dir.create(paste0(tdir, '/site'))
-dir.create(paste0(tdir, '/region'))
 
 file.copy(paste0(hur_path, '/input/ids.csv'), paste0(tdir, '/input/ids.csv'))
 file.copy(paste0(hur_path, '/input/land_water.tif'), paste0(tdir, '/input/land_water.tif'))
@@ -21,17 +19,16 @@ file.copy(paste0(hur_path, '/input/tracks.csv'), paste0(tdir, '/input/tracks.csv
 
 # test hurrecon_model_site
 test_that("hurrecon_model_site", {
-	hurrecon_model_site(hur_id="AL1935-03", site="Miami FL", msg=FALSE, hur_path=tdir)
-	output_file <- paste0(tdir, "/site/AL1935-03_Miami_FL.csv")
-	df <- read.csv(output_file)
+	df <- hurrecon_model_site(hur_id="AL1935-03", site_name="Miami FL", msg=FALSE, save=FALSE, hur_path=tdir)
 	announce_snapshot_file(name="hurrecon_model_site")
 	expect_snapshot_value(df, style="serialize", cran=FALSE, tolerance=0.001)
 })
 
 # test hurrecon_model_region
 test_that("hurrecon_model_region", {
-	hurrecon_model_region(hur_id="AL1935-03", msg=FALSE, hur_path=tdir)
-	output_file <- paste0(tdir, "/region/AL1935-03.tif")
-	expect_snapshot_file(output_file, name="hurrecon_model_region", cran=FALSE)
+	hur_r <- hurrecon_model_region(hur_id="AL1935-03", msg=FALSE, save=FALSE, hur_path=tdir)
+	wspd_matrix <- as.matrix(hur_r[[1]], wide=TRUE)
+	announce_snapshot_file(name="hurrecon_model_region")
+	expect_snapshot_value(wspd_matrix, style="serialize", cran=FALSE, tolerance=0.001)
 })
 
